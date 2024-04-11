b3cb27cd35b99af20715dd576e5844b56621c002
Olena Tr <elenaprim70@gmail.com>
2024-04-11 18:15:21 +0300

    Add project files.

---
+++ Magasin.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.CodeDom;
+using System.Diagnostics.Eventing.Reader;
+using System.Xml.Linq;
+
+namespace _0904HW
+{
+    internal class Magasine
+    {
+        public string _name { get; private set; }
+        public DateTime _year { get; private set; }
+        public string _description { get; private set; }
+        public int _numberOfPeople { get; private set; }
+        public Magasine(string name, DateTime year, string description, int numberOfPeople)
+        {
+            _name = name;
+            _year = year;
+            _description = description;
+            _numberOfPeople = numberOfPeople;
+        }
+        public Magasine() : this("no name", DateTime.Now, "no description", 0)
+        {
+        }
+
+
+        public static Magasine operator ++(Magasine magasine)
+        {
+
+            magasine._numberOfPeople = 5000 + magasine._numberOfPeople;
+
+            return magasine;
+        }
+
+        public static Magasine operator --(Magasine magasine)
+        {
+
+            magasine._numberOfPeople = magasine._numberOfPeople - 1000;
+
+            return magasine;
+        }
+
+        public static bool operator <(Magasine magasine, Magasine other)
+        {
+            if (magasine._numberOfPeople < other._numberOfPeople)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator >(Magasine magasine, Magasine other)
+        {
+            if (magasine._numberOfPeople > other._numberOfPeople)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Magasine magasine, Magasine other)
+        {
+
+            if (magasine._name == other._name && magasine._year == other._year &&
+                magasine._numberOfPeople == other._numberOfPeople)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public static bool operator !=(Magasine magasine, Magasine other)
+        {
+
+            return !(magasine == other);
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Magasine)
+            {
+                Magasine other = (Magasine)obj;
+                if (_name == other._name && _year == other._year && _numberOfPeople == other._numberOfPeople)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void ShowMagasine()
+        {
+
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine($"Year: {_year}");
+            Console.WriteLine($"Description: {_description}");
+            Console.WriteLine($"Number of Readers: {_numberOfPeople}");
+
+        }
+    }
+
+    public class Shop
+    {
+        public string _name { get; private set; }
+        public int _area { get; private set; }
+        public Shop(string name,int area)
+        { 
+            _name = name;
+            _area = area;
+        
+        }
+
+        public static Shop operator ++(Shop other)
+        {
+            other._area += 50;
+            return other;
+        }
+
+        public static Shop operator --(Shop other)
+        {
+            other._area -= 20;
+            return other;
+        }
+
+        public static bool  operator ==(Shop shop,Shop other)
+        {
+            return shop._area == other._area;
+        }
+
+        public static bool operator !=(Shop shop, Shop other)
+        {
+            return !(shop._area == other._area);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj != null)
+            {
+                if( obj is Shop )
+                {
+                    Shop other = (Shop)obj;
+                    if (other._area == _area && other._name == _name)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public void ShowShop()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine($"Area: {_area}");
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace _0904HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Magasine magasine = new Magasine("Gross Book", new DateTime(2024, 4, 10), "Description of the magazine", 15000);
+
+            Console.WriteLine();
+            Console.WriteLine("Magasin befor to increase the number of people:");
+            magasine.ShowMagasine();
+
+            magasine ++;
+
+            Console.WriteLine();
+            Console.WriteLine("Magasin after to increase the number of people:");
+            magasine.ShowMagasine();
+
+            magasine --;
+
+            Console.WriteLine();
+            Console.WriteLine("Magasin after to decrease the number of people:");
+            magasine.ShowMagasine();
+
+            Magasine magasine3 = new Magasine("Accounting", new DateTime(2022, 4, 10), "Description of the magazine", 15000);
+            Magasine magasine4 = new Magasine("Accounting work", new DateTime(2020, 4, 10), "Description of the magazine", 15000);
+
+            Console.WriteLine();
+            Console.WriteLine("Magasin 3:");
+            magasine3.ShowMagasine();
+
+            Console.WriteLine();
+            Console.WriteLine("Magasin 4:");
+            magasine4.ShowMagasine();
+
+            Console.WriteLine();
+            Console.Write("Magasine 3 == magasine4 :");
+            Console.WriteLine(magasine3 == magasine4);
+
+            Console.WriteLine();
+            Console.Write("Magasine 3 != magasine4 :");
+            Console.WriteLine(magasine3 != magasine4);
+
+            Console.WriteLine();
+            Console.Write("Magasine 3 < magasine4 :");
+            Console.WriteLine(magasine3 < magasine4);
+
+            Console.WriteLine();
+            Console.Write("Magasine 3 < magasine4 :");
+            Console.WriteLine(magasine3 < magasine4);
+
+            Console.WriteLine();
+            Console.Write("Magasine3.Equals(magasine4) :");
+            Console.WriteLine(magasine3.Equals(magasine4));
+
+            Shop shop1 = new Shop ( "Zarra", 3000 );
+
+            Console.WriteLine();
+            Console.WriteLine("Shop 1 :");
+            shop1.ShowShop();
+
+            Shop shop2 = new Shop("Mango", 3700);
+
+            Console.WriteLine();
+            Console.WriteLine("Shop 2 :");
+            shop2.ShowShop();
+
+            shop1++;
+
+            Console.WriteLine();
+            Console.WriteLine("Shop 1 :");
+            shop1.ShowShop();
+
+            shop2--;
+
+            Console.WriteLine();
+            Console.WriteLine("Shop 2 :");
+            shop2.ShowShop();
+
+            Console.WriteLine();
+
+            Console.Write($" Shops {shop1._name} == {shop2._name} :");
+            Console.WriteLine(shop1 == shop2);
+            Console.WriteLine();
+
+            Console.Write($" Shops {shop1._name} != {shop2._name} :");
+            Console.WriteLine(shop1 != shop2);
+            Console.WriteLine();
+
+            Console.Write($" Shops {shop1._name}.Equals({shop2._name}):");
+            Console.WriteLine(shop1.Equals(shop2));
+            Console.WriteLine();
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
